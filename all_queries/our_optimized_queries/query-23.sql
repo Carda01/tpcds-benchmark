419d696d26a01e47e0e16d5423605958032645c9
stef4k <stef4k@gmail.com>
2024-10-25 21:19:33 +0200

    Update query 23 to use partitions

--- all_queries/our_optimized_queries/query-23.sql
+++ all_queries/our_optimized_queries/query-23.sql
@@ -5,6 +5,7 @@ DROP TABLE if exists best_ss_customer;
 
 -- Creation of temp tables to avoid executing twice the same CTE
 -- Retrieve items sold more than 4 times across the relevant years
+-- Also use partitioned tables with d_year filtering
 CREATE TEMP TABLE frequent_ss_items AS 
 (
     SELECT 
@@ -13,20 +14,18 @@ CREATE TEMP TABLE frequent_ss_items AS
         d_date AS solddate, 
         COUNT(*) AS cnt
     FROM 
-        store_sales
+        store_sales_partitioned
     JOIN 
-        date_dim ON ss_sold_date_sk = d_date_sk
+        date_dim ON ss_sold_date_sk = d_date_sk AND d_year IN (2000, 2000 + 1, 2000 + 2, 2000 + 3)
     JOIN 
         item ON ss_item_sk = i_item_sk
-    WHERE 
-        d_year IN (2000, 2000 + 1, 2000 + 2, 2000 + 3)
     GROUP BY 
         substr(i_item_desc, 1, 30), i_item_sk, d_date
     HAVING 
         COUNT(*) > 4
 );
 
--- Calculate the maximum total sales per customer
+-- Calculate the maximum total sales per customer with use of partitioned tables with d_year filtering
 CREATE TEMP TABLE max_store_sales AS
 (
     SELECT 
@@ -37,13 +36,11 @@ CREATE TEMP TABLE max_store_sales AS
                 c_customer_sk, 
                 SUM(ss_quantity * ss_sales_price) AS csales
             FROM 
-                store_sales
+                store_sales_partitioned
             JOIN 
                 customer ON ss_customer_sk = c_customer_sk
             JOIN 
-                date_dim ON ss_sold_date_sk = d_date_sk
-            WHERE 
-                d_year IN (2000, 2000 + 1, 2000 + 2, 2000 + 3)
+                date_dim ON ss_sold_date_sk = d_date_sk AND d_year IN (2000, 2000 + 1, 2000 + 2, 2000 + 3)      
             GROUP BY 
                 c_customer_sk
         ) x
@@ -65,24 +62,24 @@ CREATE TEMP TABLE best_ss_customer AS
         SUM(ss_quantity * ss_sales_price) > (95 / 100.0) * (SELECT * FROM max_store_sales)
 );
 
--- Calculate the total sales from catalog and web sales for a specific month and year, filtered by frequent items and best customers
+-- Calculate the total sales from catalog and web sales for a specific month and year
+--filtered by frequent items and best customers and using partitioned tables with early
+-- year filtering to take advantage of partitions
 SELECT SUM(sales)
 FROM (
     SELECT cs_quantity * cs_list_price AS sales
-    FROM catalog_sales
-    JOIN date_dim ON cs_sold_date_sk = d_date_sk
-    WHERE d_year = 2000
-      AND d_moy = 3
+    FROM catalog_sales_partitioned
+    JOIN date_dim ON cs_sold_date_sk = d_date_sk AND d_year = 2000
+    WHERE d_moy = 3
       AND cs_item_sk IN (SELECT item_sk FROM frequent_ss_items)
       AND cs_bill_customer_sk IN (SELECT c_customer_sk FROM best_ss_customer)
     
     UNION ALL
     
     SELECT ws_quantity * ws_list_price AS sales
-    FROM web_sales
-    JOIN date_dim ON ws_sold_date_sk = d_date_sk
-    WHERE d_year = 2000
-      AND d_moy = 3
+    FROM web_sales_partitioned
+    JOIN date_dim ON ws_sold_date_sk = d_date_sk AND d_year = 2000 
+    WHERE d_moy = 3
       AND ws_item_sk IN (SELECT item_sk FROM frequent_ss_items)
       AND ws_bill_customer_sk IN (SELECT c_customer_sk FROM best_ss_customer)
 ) x
@@ -96,14 +93,12 @@ FROM (
         c_first_name,
         SUM(cs_quantity * cs_list_price) AS sales
     FROM 
-        catalog_sales
+        catalog_sales_partitioned
+	JOIN 
+        date_dim ON cs_sold_date_sk = d_date_sk AND d_year = 2000
     JOIN 
         customer ON cs_bill_customer_sk = c_customer_sk
-    JOIN 
-        date_dim ON cs_sold_date_sk = d_date_sk
-    WHERE 
-        d_year = 2000
-        AND d_moy = 3
+    WHERE d_moy = 3
         AND cs_item_sk IN (SELECT item_sk FROM frequent_ss_items)
         AND cs_bill_customer_sk IN (SELECT c_customer_sk FROM best_ss_customer)
     GROUP BY 
@@ -116,14 +111,12 @@ FROM (
         c_first_name,
         SUM(ws_quantity * ws_list_price) AS sales
     FROM 
-        web_sales
+        web_sales_partitioned
+	JOIN 
+        date_dim ON ws_sold_date_sk = d_date_sk AND d_year = 2000
     JOIN 
         customer ON ws_bill_customer_sk = c_customer_sk
-    JOIN 
-        date_dim ON ws_sold_date_sk = d_date_sk
-    WHERE 
-        d_year = 2000
-        AND d_moy = 3
+    WHERE d_moy = 3
         AND ws_item_sk IN (SELECT item_sk FROM frequent_ss_items)
         AND ws_bill_customer_sk IN (SELECT c_customer_sk FROM best_ss_customer)
     GROUP BY 
