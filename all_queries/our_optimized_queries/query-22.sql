8f1a8cc7d9dc9e0b249eb06c3476258a124578a9
stef4k <stef4k@gmail.com>
2024-10-25 20:39:02 +0200

    Optimize query 22

---
+++ all_queries/our_optimized_queries/query-22.sql
@@ -0,0 +1,18 @@
+-- Use explicit joins and push up filtering of month seq
+SELECT 
+    i_product_name,
+    i_brand,
+    i_class,
+    i_category,
+    AVG(inv_quantity_on_hand) AS qoh
+FROM 
+    inventory
+JOIN 
+    date_dim ON inv_date_sk = d_date_sk AND d_month_seq BETWEEN 1201 AND 1212
+JOIN 
+    item ON inv_item_sk = i_item_sk
+GROUP BY 
+    ROLLUP(i_product_name, i_brand, i_class, i_category)
+ORDER BY 
+    qoh, i_product_name, i_brand, i_class, i_category
+LIMIT 100;
